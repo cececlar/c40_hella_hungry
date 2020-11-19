d6a937d081e2d8201873cd0ce32bb82566829cd2
Cece Claridge <cece.claridge@gmail.com>
2020-11-19 15:36:52 -0500

    made API call from server.js and displayed title and image of business in app.js

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,36 @@
 import React, { useState, useEffect } from 'react';
 import { AppContextProvider } from './context/AppContext';
 import ContextDemo from './components/ContextDemo';
+import axios from 'axios';
 
 import './App.css';
 
 const App = () => {
   const [serverMessage, setServerMessage] = useState('');
+  const [businesses, setBusinesses] = useState([]);
 
-  const fetchDemoData = () => {
-    fetch('/api/demo')
-      .then((response) => response.json())
-      .then((data) => setServerMessage(data.message));
-  };
+  useEffect(() => {
+    const callOurYelpAPI = async () => {
+      try {
+        const response = await axios.get('/api/yelp');
+        setBusinesses(response.data);
+      } catch (error) {}
+    };
 
-  useEffect(fetchDemoData, []);
+    callOurYelpAPI();
+  }, []);
 
   return (
     <AppContextProvider>
-      <div id="demo">
-        <h3>Hello from client/src/App.js</h3>
-        <ContextDemo />
-        <h3>{serverMessage}</h3>
+      <div>
+        {businesses.map((business) => {
+          return (
+            <div>
+              <h1>{business.name}</h1>
+              <img src={business.image_url} />
+            </div>
+          );
+        })}
       </div>
     </AppContextProvider>
   );
--- server.js
+++ server.js
@@ -5,12 +5,24 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express');
 const path = require('path');
 const app = express();
+const axios = require('axios');
 
 // JUST FOR DEMO PURPOSES, PUT YOUR ACTUAL API CODE HERE
-app.get('/api/demo', (request, response) => {
-  response.json({
-    message: 'Hello from server.js'
-  });
+
+const getYelpAPI = async () => {
+  return axios.get(
+    'https://api.yelp.com/v3/businesses/search?location="2650 NW 5 Ave, Miami, FL 33127"&term="lunch"&radius=300',
+    { headers: { Authorization: `Bearer ${process.env.YELP_API_KEY}` } }
+  );
+};
+
+app.get('/api/yelp', async (request, response) => {
+  try {
+    const businessData = await getYelpAPI();
+    response.json(businessData.data.businesses);
+  } catch (error) {
+    console.log(error);
+  }
 });
 // END DEMO
 
